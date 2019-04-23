b13370fb985ad096e39a7612db285df45aab6e0e
Liam Roberts <liamroberts300@gmail.com>
2019-04-23 10:27:49 +0100

    added axios

--- src/components/MarsRover.js
+++ src/components/MarsRover.js
@@ -1,9 +1,33 @@
 import React from 'react'
 
+import axios from 'axios'
+
 class MarsRover extends React.Component{
+
+  constructor() {
+    super()
+
+    this.state = {
+      data: {}
+    }
+  }
+
+  componentDidMount(){
+    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=0NeW5J5VXCp6NRbCOu6Yv5ANYejzU73uezmTIfTv')
+      .then(res => this.setState({data: res.data }))
+      // <img src={this.state.data.photos[0].img_src} alt='test image' />
+  }
+
+
   render(){
+    console.log(this.state.data)
     return(
-      <h1>Mars Rover</h1>
+      <section className='section'>
+        <div className='container'>
+          <h1>Hello</h1>
+
+        </div>
+      </section>
     )
   }
 }
