c8e134ca776408bc21c8b9be8bf243d82a14fc56
Liam Roberts <liamroberts300@gmail.com>
2019-04-24 15:52:52 +0100

    final commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,7 @@ const Home = () => {
   return(
     <section className='section home-container'>
       <div className='container'>
-        <div className='nasaLogo'></div>
-        <h1 className='title is-1'>🛰 🚀 🛸</h1>
+        <h1 className='title'>🛰 🚀 🛸</h1>
         <h2 className='subtitle is-3 has-text-white has-text-weight-bold'>Please choose your interstellar adventure!</h2>
         <div className='columns is-centered is-multiline'>
           <div className='column is-5'>
