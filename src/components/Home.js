1a81e1fa4750927f98b105c41913fc6469fcce6a
Liam Roberts <liamroberts300@gmail.com>
2019-04-24 15:49:15 +0100

    checked over project for any errors and for styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 const Home = () => {
   return(
-    <section className='section'>
+    <section className='section home-container'>
       <div className='container'>
-        <h1 className='title is-1'>NASA Mars Rover Photos</h1>
+        <div className='nasaLogo'></div>
+        <h1 className='title is-1'>🛰 🚀 🛸</h1>
+        <h2 className='subtitle is-3 has-text-white has-text-weight-bold'>Please choose your interstellar adventure!</h2>
+        <div className='columns is-centered is-multiline'>
+          <div className='column is-5'>
+            <Link className='button is-dark is-large' to='/marsrover'>Mars Rover Images</Link>
+          </div>
+          <div className='column is-5'>
+            <Link className='button is-dark is-large' to='/apodindex'>Astronomy Pictures</Link>
+          </div>
+        </div>
       </div>
     </section>
   )
