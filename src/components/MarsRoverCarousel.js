b38f6c4fd5e59b8d8a32092f86a2157d2930aa26
Liam Roberts <liamroberts300@gmail.com>
2019-04-24 12:17:43 +0100

    added modals and random images in carousel

--- src/components/MarsRoverCarousel.js
+++ src/components/MarsRoverCarousel.js
@@ -6,6 +6,7 @@ class MarsRoverCarousel extends React.Component{
 
   constructor(){
     super()
+
     this.state = {
       active: false,
       date: new Date('2019-03-31'),
@@ -14,8 +15,9 @@ class MarsRoverCarousel extends React.Component{
 
     this.updateImages = this.updateImages.bind(this)
     this.toggleActive = this.toggleActive.bind(this)
-
+    this.randomDatePicker = this.randomDatePicker.bind(this)
   }
+
 
   toggleActive() {
     this.setState({ active: !this.state.active })
@@ -25,10 +27,10 @@ class MarsRoverCarousel extends React.Component{
     this.updateImages()
   }
 
-  updateImages(){
+  updateImages(randomDate = null){
     axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos', {
       params: {
-        earth_date: this.state.date,
+        earth_date: randomDate || this.state.date,
         page: 1,
         api_key: '0NeW5J5VXCp6NRbCOu6Yv5ANYejzU73uezmTIfTv'
       }
@@ -36,10 +38,21 @@ class MarsRoverCarousel extends React.Component{
       .then(res => this.setState({data: res.data.photos },))
   }
 
+  randomDatePicker(){
+    const randomYear = Math.floor(Math.random() * 7)
+    const randomMonth = Math.ceil(Math.random() * 12)
+    const randomDay = Math.ceil(Math.random() * 28)
+    const year = 2012 + randomYear
+    const randomDate = `${year}-${randomMonth}-${randomDay}`
+
+    this.setState({date: randomDate}, () => this.updateImages(randomDate))
+  }
+
   render(){
     return(
       <section className='section'>
         <div className='container'>
+          <button className='button is-dark' onClick={this.randomDatePicker}>More Photos</button>
           <div className='columns is-multiline'>
             {this.state.data.map(datum =>
               <div key={datum.id} className='column is-one-quarter-desktop is-one-third-tablet'>
@@ -55,7 +68,6 @@ class MarsRoverCarousel extends React.Component{
                   </div>
                   <button className="modal-close is-large" aria-label="close" onClick={this.toggleActive}></button>
                 </div>
-
               </div>
             ).splice(0, 4)}
           </div>
